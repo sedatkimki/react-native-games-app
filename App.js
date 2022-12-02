a68dd4b77377cbfa214bd8176096fdccc2b3ae58
sedat <korkmz.sedat@gmail.com>
2022-12-02 21:49:55 +0300

    feature/navigation - add tab navigation

--- App.js
+++ App.js
@@ -3,11 +3,18 @@ import { StyleSheet, Text, View } from "react-native";
 import Games from "./src/screens/games";
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Favourites from "./src/screens/favourites";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Games />
+      <Tab.Navigator>
+        <Tab.Screen name="Games" component={Games} />
+        <Tab.Screen name="Favourites" component={Favourites} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
